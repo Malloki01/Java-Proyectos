96a37589071bb3e6c32fd3ff41670c22b1a01300
Rodrigo <r.ms01@hotmail.com>
2024-08-19 15:58:59 -0500

    Proyecto Calculadora: avance 2

--- CalculadoraApp/src/CalculadoraApp.java
+++ CalculadoraApp/src/CalculadoraApp.java
@@ -1,43 +1,53 @@
 import java.util.Scanner;
 
 public class CalculadoraApp {
-    public static void main(String[] args){
-        System.out.println("**** Aplicacion Calculadora ****");
+    public static void main(String[] args) {
         Scanner consola = new Scanner(System.in);
-        //Mostrar Menu
-        System.out.println("1.Suma \n2.Resta \n3.Multiplicación \n4.División \n5.Salir");
-        System.out.print("Operación a realizar: ");
-        var operacion = Integer.parseInt(consola.nextLine());
-        if(operacion>=1 && operacion<=4){
-            System.out.println("Proporciona el primer valor");
-            var num1 = Integer.parseInt(consola.nextLine());
-            System.out.println("Proporciona el segundo valor");
-            var num2 = Integer.parseInt(consola.nextLine());
-            int resultado;
+        while (true) {
+            System.out.println("**** Aplicacion Calculadora ****");
+            //Mostrar Menu
+            System.out.println("1.Suma \n2.Resta \n3.Multiplicación \n4.División \n5.Salir");
+            System.out.print("Operación a realizar: ");
+            try {
+                var operacion = Integer.parseInt(consola.nextLine());
+                if (operacion >= 1 && operacion <= 4) {
+                    System.out.println("Proporciona el primer valor");
+                    var num1 = Integer.parseInt(consola.nextLine());
+                    System.out.println("Proporciona el segundo valor");
+                    var num2 = Integer.parseInt(consola.nextLine());
+                    int resultado;
 
-            switch (operacion){
-                case 1:
-                    resultado = num1+num2;
-                    System.out.println("Resultado Suma: "+resultado);
+                    switch (operacion) {
+                        case 1:
+                            resultado = num1 + num2;
+                            System.out.println("Resultado Suma: " + resultado);
+                            break;
+                        case 2:
+                            resultado = num1 - num2;
+                            System.out.println("Resultado Resta: " + resultado);
+                            break;
+                        case 3:
+                            resultado = num1 * num2;
+                            System.out.println("Resultado Multiplicacion: " + resultado);
+                            break;
+                        case 4:
+                            resultado = num1 / num2;
+                            System.out.println("Resultado Suma: " + resultado);
+                            break;
+                        default:
+                            System.out.println("Operación Erronea: " + operacion);
+                    }
+
+                } else if (operacion == 5) {
+                    System.out.println("Vuelva pronto...");
                     break;
-                case 2:
-                    resultado = num1-num2;
-                    System.out.println("Resultado Resta: "+resultado);
-                    break;
-                case 3:
-                    resultado = num1*num2;
-                    System.out.println("Resultado Multiplicacion: "+resultado);
-                    break;
-                case 4:
-                    resultado = num1/num2;
-                    System.out.println("Resultado Suma: "+resultado);
-                    break;
-                default:
-                    System.out.println("Operación Erronea: "+operacion);
+                } else {
+                    System.out.println("Operación Erronea: " + operacion);
+                }
+                System.out.println();
+            }catch (Exception e){
+             System.out.println("Ocurrió un error:"+e.getMessage());
             }
-
-        } else if (operacion== 5) {
-            System.out.println("Vuelva pronto...");
-        }else { System.out.println("Operación Erronea: "+operacion);}
+        } // while
     }
 }
